86bd6017aec435efd087f1ada776c6c21bbaca85
Alex Chiurtu <alex.chiurtu@gmail.com>
2018-12-09 19:51:12 +0200

    Add files via upload

---
+++ Lab 5/autokey cipher.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+    char msg[] = "ANAAREMERE";
+    char key[] = "MIHAI";
+    int msgLen = strlen(msg), keyLen = strlen(key), i, j, k;
+    
+    char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
+    
+    //generating new key
+    for(i = 0, j = 0, k = 0; i < msgLen; ++i, ++j){
+        if(j >= keyLen)
+        {
+            newKey[i] = msg[k];
+            ++k;
+        }
+        else
+            newKey[i] = key[j];
+    }
+    
+    newKey[i] = '\0';
+    
+    //encryption
+    for(i = 0; i < msgLen; ++i)
+        encryptedMsg[i] = ((newKey[i] - msg[i] + 26) % 26) + 'A';
+    
+    encryptedMsg[i] = '\0';
+    
+    //decryption
+    for(i = 0; i < msgLen; ++i)
+        decryptedMsg[i] = (((newKey[i] - encryptedMsg[i] ) + 26) % 26) + 'A';
+    
+    decryptedMsg[i] = '\0';
+    
+    cout<<"Original Message: "<<msg;
+    cout<<"\nKey: "<<key;
+    cout<<"\nNew Generated Key: "<<newKey;
+    cout<<"\nEncrypted Message: "<<encryptedMsg;
+    cout<<"\nDecrypted Message: "<<decryptedMsg;
+    
+    return 0;
+}
